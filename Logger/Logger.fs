a795dd5e0a10d3a0253ead60622445c6bdb58f25
kunzimariano <kunzi.mariano@gmail.com>
2014-05-27 10:27:51 -0300

    -Removed azure library dependency. -Middleware now takes the sumo logic url as a parameter instead of reading it directly.

--- Logger/Logger.fs
+++ Logger/Logger.fs
@@ -2,58 +2,74 @@
 
 open System
 open System.Text
+open System.IO
 open System.Threading.Tasks
 open RestSharp
-open Microsoft.Owin
-open Microsoft.WindowsAzure.ServiceRuntime
+open System.Collections.Generic
+open System.Linq
 
-type Logger(next : OwinMiddleware) = 
-    inherit OwinMiddleware(next)
+type appFunc = Func<IDictionary<string,Object>,Task>
+
+type Logger(next:appFunc, sumoLogicUrl:string) =    
     
-    let postToSumoLogic(content : string) =        
-        let client = new RestClient()
-        client.BaseUrl <- RoleEnvironment.GetConfigurationSettingValue "SumoLogic.Url"
-        let request = new RestRequest()        
-        request.Method <- Method.POST        
-        (request.AddFile("ParameterName", Encoding.UTF8.GetBytes(content), "FileName")) |> ignore
-        client.ExecuteTaskAsync(request) |> Async.AwaitTask
-
     let getCurrentUTCDateAndTime() =
         let now = System.DateTimeOffset.Now.ToUniversalTime()
         let date = now.ToString("MM/dd/yy")
         let time = now.ToString("H:mm:ss")
-        date, time        
-
-    let getLogContent (request:IOwinRequest) (response:IOwinResponse) =
-        let success, ua = request.Headers.TryGetValue "User-Agent"
-        let userAgent =            
-            if success then ua |> Array.reduce(fun acc elem -> acc + elem) |> System.Web.HttpUtility.UrlEncode else "-"
+        date, time
         
-        let responseLength = 
-            if response.ContentLength.HasValue then response.ContentLength.Value.ToString() else  "-"
+    let getUserAgent (headers: IDictionary<string, string[]>) =
+        let success, ua = headers.TryGetValue "User-Agent"
+        if success then ua |> Array.reduce(fun acc elem -> acc + elem) |> System.Web.HttpUtility.UrlEncode else "-"
 
-        let requestLength = 
-            if request.Body.CanSeek then request.Body.Length.ToString() else "-"
-        
+    let getContentLength (headers: IDictionary<string, string[]>) =
+        let success, length = headers.TryGetValue "Content-Length"        
+        if success then length.First() else "0"
+
+    let getStreamLength (stream:Stream) = if stream.CanSeek then stream.Length.ToString() else "-"
+
+    let getLogContent (environment:IDictionary<string,Object>) =        
+        let userAgent = environment.["owin.RequestHeaders"] :?> IDictionary<string, string[]> |> getUserAgent
+        let requestLength = environment.["owin.RequestBody"] :?> Stream |> getStreamLength
+        let responseLength = environment.["owin.ResponseHeaders"] :?> IDictionary<string, string[]> |> getContentLength
+        let requestMethod = environment.["owin.RequestMethod"] :?> String
+        let requestPath = environment.["owin.RequestPath"] :?> String
+        let requestPathBase = environment.["owin.RequestPathBase"] :?> String
+        let remoteIpAddress = environment.["server.RemoteIpAddress"] :?> String
+        let localIpAddress = environment.["server.LocalIpAddress"] :?> String        
+        let responseStatusCode = environment.["owin.ResponseStatusCode"].ToString()                                               
         let date, time = getCurrentUTCDateAndTime()
         
-        sprintf "%s %s %s %s %s %s %s %d %s %s" 
+        sprintf "%s %s %s %s %s %s %s %s %s %s"
             date 
             time 
-            request.Method 
-            request.Host.Value 
-            request.Path.Value 
-            request.RemoteIpAddress 
+            requestMethod 
+            localIpAddress
+            requestPath 
+            remoteIpAddress
             userAgent 
-            response.StatusCode 
+            responseStatusCode 
             responseLength 
             requestLength
 
-    let asyncInvoke (context:IOwinContext) = async {        
-        let! r = next.Invoke context |> Async.AwaitIAsyncResult
-        let content = getLogContent context.Request context.Response        
-        return! postToSumoLogic content         
+
+    member x.Next = next
+    member x.SumoLogicUrl = sumoLogicUrl
+
+    member x.PostToSumoLogic(content : string) =        
+        let client = new RestClient()
+        client.BaseUrl <- x.SumoLogicUrl
+        client.UserAgent <- "Openagile.Sumologic.Logger"
+        let request = new RestRequest()        
+        request.Method <- Method.POST        
+        (request.AddFile("ParameterName", Encoding.UTF8.GetBytes(content), "FileName")) |> ignore
+        client.ExecuteTaskAsync(request) |> Async.AwaitTask
+        
+    member x.AsyncInvoke (environment:IDictionary<string,Object>) = async {
+        let! r = x.Next.Invoke environment |> Async.AwaitIAsyncResult        
+        let content = getLogContent environment
+        return! x.PostToSumoLogic content         
     }
-                                
-    override x.Invoke(context : IOwinContext) = 
-        asyncInvoke context |> Async.StartAsTask :> Task
+    
+    member x.Invoke(environment :IDictionary<string,Object>) = 
+        x.AsyncInvoke environment |> Async.StartAsTask :> Task
